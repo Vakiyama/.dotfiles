a784fa1ff1eab5331d00abe594a6dc98d844819e
Vakiyama <vakiyama@outlook.com>
2024-02-03 00:19:53 -0800

    customize fzf + sessionizer addition

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -61,8 +61,12 @@
 #  /etc/profiles/per-user/Root/etc/profile.d/hm-session-vars.sh
 #
 # if you don't want to manage your shell through Home Manager.
-    home.sessionVariables = {
+    systemd.user.sessionVariables = {
         EDITOR = "nvim";
+        FZF_DEFAULT_OPTS=" \
+          --color=bg+:#414559,bg:#303446,spinner:#f2d5cf,hl:#e78284 \
+          --color=fg:#c6d0f5,header:#e78284,info:#ca9ee6,pointer:#f2d5cf \
+          --color=marker:#f2d5cf,fg+:#c6d0f5,prompt:#ca9ee6,hl+:#e78284";
     };
 
     programs.bash = {
