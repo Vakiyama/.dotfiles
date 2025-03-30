ee56920e969668a46237922ccee7a5abb1c7f075
Vakiyama <vakiyama@outlook.com>
2025-03-29 22:23:17 -0700

    fix: blink.cmp fuzzy issues

--- nvim/after/plugin/blink.lua
+++ nvim/after/plugin/blink.lua
@@ -1,3 +1,17 @@
 require('blink.cmp').setup({
-  fuzzy = { implementation = "prefer_rust_with_warning" }
+  fuzzy = {
+    implementation = "prefer_rust_with_warning",
+    prebuilt_binaries = {
+      download = false,
+    }
+  },
 })
+-- The 'detect_cwd' field is deprecated and no longer has any affect.
+-- See https://github.com/epwalsh/obsidian.nvim/pull/366 for more details.
+-- [blink.cmp]: No fuzzy matching library found
+-- [blink.cmp]: Falling back to Lua implementation due to error while downloading pre-built binary:
+-- ...er/start/blink.cmp/lua/blink/cmp/fuzzy/download/init.lua:77: No fuzzy matching library found, but downloading from github is disabled.
+-- Either run `cargo build --release` via your package manager, or set `fuzzy.prebuilt_binaries.download = true` in config.
+-- See the docs for more info.
+-- [blink.cmp]: Set `fuzzy.implementation = 'prefer_rust' | 'lua'` to suppress this warning.
+
