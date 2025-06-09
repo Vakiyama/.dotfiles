ebd272249c640cf4ca9b08f130571950250d3805
Vakiyama <vakiyama@outlook.com>
2025-06-08 17:40:32 -0700

    feat: finalize config

--- home-manager/shared/apps/default.nix
+++ home-manager/shared/apps/default.nix
@@ -1,30 +1,71 @@
 { pkgs, inputs, ... }:
 {
   imports = [
-    ./bash.nix
+    ./fish.nix
     ./nvim.nix
     ./scripts.nix
+    ./direnv.nix
+    ./git.nix
   ];
 
-  home.packages = with pkgs; [
-    xborders
-    # vm stuff
-    virt-viewer
-    spice
-    spice-gtk
-    spice-protocol
-    win-virtio
-    win-spice
-    adwaita-icon-theme
-    hyprpaper
-    brightnessctl
-    wl-clipboard
-    git-credential-manager
-    xfce.thunar
-    bluetuith
-    deno
+  nixpkgs.config =
+    {
+      allowUnfree = true;
+    };
 
-    inputs.zen-browser.packages.${pkgs.system}.specific
+  home.packages = with pkgs;
+    [
+      adwaita-icon-theme
+      brightnessctl
+      git-credential-manager
+      xfce.thunar
+      bluetuith
 
-  ];
+      google-chrome
+      discord
+      spotify
+      neofetch
+      feh
+      obsidian
+      slack
+      ripgrep
+      zlib
+      yq-go
+      pciutils
+      tridactyl-native
+      steam
+      steam-run
+      vulkan-tools
+      mysql-workbench
+      shutter
+      lutris
+      ngrok
+      r2modman
+
+      htop-vim
+      prettierd
+
+      eza # better ls (exa)
+      rm-improved # safer rm (rip)
+      # unstable.kdenlive
+      obs-studio
+      pgmodeler
+      pulseeffects-legacy
+
+      ytfzf
+      ueberzugpp
+      mpv
+      python3
+      luajitPackages.luarocks
+      unzip
+      pavucontrol
+      earbuds
+      openal
+
+
+      kitty
+      ripgrep
+
+      inputs.zen-browser.packages.${pkgs.system}.specific
+    ];
 }
--- home-manager/shared/apps/git.nix
+++ home-manager/shared/apps/git.nix
@@ -1,17 +1,9 @@
 { ... }:
 {
-  programs.bash = {
+  programs.git = {
     enable = true;
-    git = {
-      enable = true;
-      extraConfig = {
-        credential.helper = "store";
-      };
-    };
-    direnv = {
-      enable = true;
-      enableBashIntegration = true; # see note on other shells below
-      nix-direnv.enable = true;
+    extraConfig = {
+      credential.helper = "store";
     };
   };
 }
