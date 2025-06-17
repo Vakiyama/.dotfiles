80b8306a7fd21d887b7d4e951731171af699b9d3
Vakiyama <vakiyama@outlook.com>
2025-06-17 16:53:58 -0700

    fix: aliases in wrong place

--- home-manager/shared/apps/fish.nix
+++ home-manager/shared/apps/fish.nix
@@ -5,5 +5,11 @@
     interactiveShellInit = ''
       set fish_greeting # Disable greeting
     '';
+    shellAliases = {
+      rm = "rip";
+      vi = "nvim";
+      ls = "exa --icons -F -H --group-directories-first --git -1";
+      lt = "exa --tree --level=2 --long --icons --git";
+    };
   };
 }
